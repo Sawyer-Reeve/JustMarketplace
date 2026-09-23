c65bd03cdfde15eb3a7544fc7407b96d1577462b
Daytrippr <60251063+Daytrippr@users.noreply.github.com>
2026-09-23 14:54:15 -0600

    Restrict Navigation to Marketplace
    added interval loop to repeatedly check the page we are on

--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-console.log("JustMarketplace is running.");
+const MARKETPLACE_PATH = "/marketplace";
+
+function checkPage() {
+    const path = window.location.pathname;
+
+    if (!path.startsWith(MARKETPLACE_PATH)) {
+        window.location.replace(
+            "https://www.facebook.com/marketplace"
+        );
+    }
+}
+
+checkPage();
+
+setInterval(checkPage, 500);
